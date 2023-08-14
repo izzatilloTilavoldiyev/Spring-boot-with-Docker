2d21ef74fdc775f4aa9fe48cda24b192acaef003
izzatillotilavoldiyev5 <izzatillotilavoldiyev5@gmail.com>
2023-08-14 19:09:28 +0500

    updated

---
+++ src/main/java/uz/pdp/springbootwithdocker/entity/UserEntity.java
@@ -0,0 +1,23 @@
+package uz.pdp.springbootwithdocker.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserEntity {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String name;
+}
